21097a01e516bcbaf72f4c21e907bc9d9d737815
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-03-11 22:10:10 +0900

    deviceによるログイン機能の実装のためのマイグレーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  namespace :v1, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+    resource :users, only: [:create]
+  end
 end
