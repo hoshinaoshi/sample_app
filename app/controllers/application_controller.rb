3ef51721ec278dd3d65e9a1c096c15bb409abf2c
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-03-18 00:03:59 +0900

    シングルクォートをダブルクォートに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
   # User Authentication
   # Authenticates the user with OAuth2 Resource Owner Password Credentials
   def authenticate_user_from_token!
-    auth_token = request.headers['Authorization']
+    auth_token = request.headers["Authorization"]
 
     if auth_token
       authenticate_with_auth_token auth_token
@@ -21,12 +21,12 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_with_auth_token auth_token
-    unless auth_token.include?(':')
+    unless auth_token.include?(":")
       authenticate_error
       return
     end
 
-    user_id = auth_token.split(':').first
+    user_id = auth_token.split(":").first
     user = User.where(id: user_id).first
 
     if user && Devise.secure_compare(user.access_token, auth_token)
@@ -41,6 +41,6 @@ class ApplicationController < ActionController::API
   # Authentication Failure
   # Renders a 401 error
   def authenticate_error
-    render json: { error: t('devise.failure.unauthenticated') }, status: 401
+    render json: { error: t("devise.failure.unauthenticated") }, status: 401
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :users
   namespace :v1, defaults: { format: :json } do
-    resource :login, only: [:create], controller: :sessions
-    resources :users, only: [:index, :create]
+    resource :login, only: %i(create), controller: :sessions
+    resources :users, only: %i(index create)
   end
 end
