a398abe57f8b18002cf3bccca7d83df1ecc378d9
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-03-24 04:06:38 +0900

    検索処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users
   namespace :v1, defaults: { format: :json } do
     resource :sessions, only: %i(create)
     resources :users, only: %i(index create)
