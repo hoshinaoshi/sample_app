258eed4dd0d98f996b8b70a5ebe2e64995ef0a42
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-03-24 19:43:28 +0900

    検索機能追加

--- app/serializers/v1/user_serializer.rb
+++ app/serializers/v1/user_serializer.rb
@@ -1,5 +1,9 @@
 module V1
   class UserSerializer < ActiveModel::Serializer
-    attributes :id, :email, :created_at, :updated_at
+    attributes :id, :nickname, :profile_image_url, :residence
+
+    def profile_image_url
+      "#{EasySettings.s3_bucket}/profile/#{object.main_image}"
+    end
   end
 end
